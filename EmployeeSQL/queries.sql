ebeac1e6c0e1b1f80f3151918678bd7e7662d185
Michelle Herman <michelleherman@Michelles-iMac.attlocal.net>
2021-08-31 04:07:19 -0500

    progress

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -11,19 +11,37 @@ WHERE EXTRACT(YEAR FROM hire_date)=1986;
 
 --List the manager of each dept with: dept #, dept name, the manager's emp#, last&first name
 SELECT a.dept_no, b.dept_name, a.emp_no, c.last_name, c.first_name
-FROM dept_manager a
-JOIN departments b
+FROM dept_manager AS a
+JOIN departments AS b
 ON a.dept_no=b.dept_no
-JOIN employees c
+JOIN employees AS c
 ON a.emp_no=c.emp_no;
 
-
 --List the dept of each emp with the following info: emp#, last name, first name, and dept name
+SELECT a.emp_no, a.last_name, a.first_name, b.dept_name 
+FROM employees as a
+LEFT JOIN dept_employee b
+ON a.emp_no=b.emp_no; 
 
---List first name, last name, and sex for emp whose first name is "Hercules" and last names begin with "B.
+--List first name, last name, and sex for emp whose first name is "Hercules" and last names begin with "B"
+SELECT first_name, last_name, sex 
+from employees 
+where first_name ='Hercules'
+and last_name LIKE 'B%'
 
---List all emp in the Sales dept, including their employee number, last name, first name, and dept name.
+--List all emp in the Sales dept, including their employee number, last name, first name, and dept name
+SELECT a.emp_no, a.last_name, a.first_name, b.dept_name
+FROM employees AS a
+LEFT JOIN dept_employee AS b
+ON a.emp_no=b.emp_no
+WHERE b.dept_name='Sales';
 
---List all emp in the Sales and Development dept, including their emp#, last name, first name, and dept name.
+--List all emp in the Sales and Development dept, including their emp#, last name, first name, and dept name
+
 
 --In descending order, list the frequency count of emp last names, i.e., how many emp share each last name
+SELECT last_name, count(*)
+AS "frequency_of_last_names"
+FROM employees
+GROUP BY last_name
+ORDER BY "frequency_of_last_names" DESC;
